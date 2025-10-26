4ce2f0f63db47804da82908d5f5a32d126cc17cc
Ahmed Hussien <ahmedhussien6665@gmail.com>
2025-10-26 12:36:31 -0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,28 @@
+// Person.java - Abstract class for inheritance
+public abstract class Person {
+    protected String name;
+    protected int age;
+    protected String gender;
+    
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+    
+    // Abstract method
+    public abstract void displayInfo();
+    
+    // Getters
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public String getGender() {
+        return gender;
+    }
+}
